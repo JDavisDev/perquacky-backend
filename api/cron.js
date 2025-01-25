f043148dc921ac0182043788a539ae7671deb141
Jordan Davis <jdavisdev@gmail.com>
2025-01-25 14:46:50 -0700

    add another letter

--- api/cron.js
+++ api/cron.js
@@ -45,6 +45,7 @@ function generateLetterSet() {
   const diceThree = ["B", "H", "I", "K", "R", "T"];
   const diceFour = ["F", "H", "I", "R", "S", "U"];
   const diceFive = ["G", "I", "M", "R", "S", "U"];
+  const diceSix = ["E", "J", "Q", "V", "P", "R"];
   const diceSeven = ["F", "I", "N", "P", "T", "E"];
   const diceEight = ["C", "M", "O", "O", "P", "W"];
   const diceNine = ["D", "L", "N", "O", "R", "T"];
@@ -55,6 +56,7 @@ function generateLetterSet() {
     diceThree,
     diceFour,
     diceFive,
+    diceSix,
     diceSeven,
     diceEight,
     diceNine,
--- api/index.js
+++ api/index.js
@@ -6,53 +6,12 @@ import "./env.js";
 app.use(cors());
 require("dotenv").config();
 
-// cron.schedule("0 45 13 * * *", async () => {
-//   console.log("entered cron")
-//   setTodayLetters();
-//   console.log("exit cron");
-//  }, { timezone: "America/New_York" });
-
 const uri = process.env.MONGODB_URL;
 const client = new MongoClient(uri);
-async function addTodaysDateToDb() {
-  try {
-    await client.connect();
-    const db = client.db("quackle");
-    const collection = db.collection("days");
-    // official current puzzle day
-    const options = {
-      timeZone: "America/New_York",
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    };
-    const formatter = new Intl.DateTimeFormat("en-US", options);
-    const easternTime = formatter.format(new Date());
-
-    const doc = { date: easternTime };
-    const result = await collection.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } finally {
-    // Ensures that the client will close when you finish/error
-  }
-}
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-
-app.get("/today", (req, res) => {
-  getTodaysDateEastern(res);
-});
-
-async function getTodaysDateEastern(res) {
-  try {
-    await setTodayLetters();
-    res.status(200).send("done");
-  } catch (e) {
-    console.error(e);
-  }
-}
 
 function getToday() {
   // official current puzzle day
@@ -64,16 +23,6 @@ function getToday() {
   };
   const formatter = new Intl.DateTimeFormat("en-US", options);
   return formatter.format(new Date());
-}
-
-async function setTodayLetters() {
-  console.log("running set today letters");
-  const today = getToday();
-  const letters = generateLetterSet();
-  // insert letters into today if they are unique
-  await addLettersToDb(letters, today);
-  console.log(`today letters are: ${today} : ${letters.join("")}`);
-  return;
 }
 
 // Function to get random characters
@@ -89,21 +38,6 @@ function getRandomCharacters(matrix) {
   });
 
   return returnArray;
-}
-
-async function addLettersToDb(letters, today) {
-  try {
-    await client.connect();
-    const db = client.db("quackle");
-    const collection = db.collection("days");
-
-    const doc = { date: today, letters: letters.join("") };
-    const result = await collection.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    client.close();
-  }
 }
 
 async function getLettersInDb() {
@@ -142,79 +76,6 @@ async function getLettersInDb() {
   } finally {
     // Ensures that the client will close when you finish/error
   }
-}
-
-function generateLetterSet() {
-  const vowels = ["A", "E", "I", "O", "U"];
-  const consonants = [
-    "B",
-    "C",
-    "D",
-    "F",
-    "G",
-    "H",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "V",
-    "W",
-    "X",
-    "Y",
-  ];
-  const alphabetWithoutZ = [...vowels, ...consonants];
-
-  // Randomly determine the number of vowels (between 2 and 5)
-  const numVowels = Math.floor(Math.random() * 4) + 2; // 2 to 5 vowels
-  const numConsonants = 9 - numVowels;
-
-  // Randomly pick vowels
-  const selectedVowels = [];
-  while (selectedVowels.length < numVowels) {
-    const randomVowel = vowels[Math.floor(Math.random() * vowels.length)];
-    selectedVowels.push(randomVowel);
-  }
-
-  // Randomly pick consonants, ensuring Q includes U
-  const selectedConsonants = [];
-  let hasQ = false;
-
-  while (selectedConsonants.length < numConsonants) {
-    const randomConsonant =
-      consonants[Math.floor(Math.random() * consonants.length)];
-    if (randomConsonant === "Q") {
-      if (!selectedVowels.includes("U")) selectedVowels.push("U"); // Ensure U is added with Q
-      hasQ = true;
-    }
-    selectedConsonants.push(randomConsonant);
-  }
-
-  // Combine and shuffle letters
-  let selectedLetters = [...selectedVowels, ...selectedConsonants];
-  for (let i = selectedLetters.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [selectedLetters[i], selectedLetters[j]] = [
-      selectedLetters[j],
-      selectedLetters[i],
-    ];
-  }
-
-  if (selectedConsonants.length === numConsonants + numVowels + 1) {
-    // If we have an extra letter, remove it
-    selectedConsonants
-      .filter((letter) => letter !== "Q" && letter !== "U")
-      .pop();
-  }
-
-  selectedLetters = [...selectedVowels, ...selectedConsonants];
-
-  return selectedLetters;
 }
 
 async function onLettersGet(res) {
